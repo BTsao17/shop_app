3ef96c31060b7815d889ee6003224205ed15625a
Bonnie Tsao <IceJade1788@gmail.com>
2019-01-23 14:50:14 -0800

    separated product list into own file

--- shop-backend/server.js
+++ shop-backend/server.js
@@ -2,89 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const products = require('./data-files/products') //importing products from a separate file 
 
 app.use(cors())
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json())
-
-const products = {
-  inkProducts: [{
-      name: 'Sailor Jentle - Yuki-Akari',
-      price: 24.75,
-      picture: '/images/inks/Sailor_YukiAkari.jpg',
-      type: 'ink'
-    },
-    {
-      name: 'Pilot Iroshizuku - Yama-budo',
-      price: 28,
-      picture: '/images/inks/Pilot_YamaBudo.jpg',
-      type: 'ink'
-    },
-    {
-      name: 'J.Herbin - 1670 Stormy Grey',
-      price: 27.50,
-      picture: '/images/inks/JHerbin_StormyGrey.jpg',
-      type: 'ink'
-    },
-    {
-      name: `Noodler's - Apache Sunset`,
-      price: 16.50,
-      picture: '/images/inks/Noodler_ApacheSunset.jpg',
-      type: 'ink'
-    },
-    {
-      name: 'Pilot Iroshizuku - Chiku-Rin',
-      price: 28,
-      picture: '/images/inks/Pilot_ChikuRin.jpg',
-      type: 'ink'
-    },
-    {
-      name: 'Diamine - Ochre',
-      price: 14,
-      picture: '/images/inks/Diamine_Ochre.jpg',
-      type: 'ink'
-    }
-  ],
-  penProducts: [{
-      name: 'Pilot Metropolitan',
-      price: 21.50,
-      picture: '/images/pens/Pilot_Metropolitan.jpg',
-      type: 'pen'
-    },
-    {
-      name: 'TWSBI ECO - White',
-      price: 30,
-      picture: '/images/pens/TWSBI_Eco.jpg',
-      type: 'pen'
-    },
-    {
-      name: 'Faber-Castell Ambition - Water Lily',
-      price: 100,
-      picture: '/images/pens/Faber_Ambition.jpg',
-      type: 'pen'
-    },
-    {
-      name: 'Kaweco Liliput - Fireblue',
-      price: 212.50,
-      picture: '/images/pens/Kaweco_Liliput.jpg',
-      type: 'pen'
-    },
-    {
-      name: 'Sailor 1911S - Stormy Sea',
-      price: 265,
-      picture: '/images/pens/Sailor_1911S.jpg',
-      type: 'pen'
-    },
-    {
-      name: 'Kaweco Skyline Sport - Grey',
-      price: 28.75,
-      picture: '/images/pens/Kaweco_Skyline.jpg',
-      type: 'pen'
-    }
-  ]
-}
 
 let shoppingCart = []
 
@@ -102,7 +26,7 @@ app.get('/cart', (req, res) => {
   res.json(shoppingCart)
 });
 
-app.post('/clear', (req, res) => {
+app.delete('/clear', (req, res) => {
   shoppingCart = req.body
   res.send('cleared shopping cart on logout')
 });
